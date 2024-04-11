d9371e5c2e70e82eca73f22841b4c94051d55858
Arauz <86898345+kjarj54@users.noreply.github.com>
2024-04-10 20:52:07 -0600

    --Display in progress

---
+++ geminiflow/src/components/Display.tsx
@@ -0,0 +1,12 @@
+
+function Display() {
+    return (
+        <div className="max-w-4x1 mx-auto p-4">
+            <div className="flex flex-col gap-6">
+
+            </div>
+        </div>
+    );
+}
+
+export default Display;
